84a298416e779a71212ad73d170a28d6302bd243
Shekeeb <shekeebmohammed10@gmailcom>
2024-08-01 11:25:18 +0530

    Express

---
+++ Day28/day28.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send('Welcome');
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "form.html"));
+});
+
+//Middleware  To perform something just after request or just before response.ie, authentication,cookies,sessions...etc
+const Middleware=(req,res,next)=>{
+    console.log("Login done")
+    next()
+}
+
+app.use(Middleware)
+
+app.post("/loginaction", (req, res) => {
+  const { name, place } = req.body;
+  res.send(`
+    <h1>Login Successfull</h1>
+    <h2>I'm ${name} from ${place}</h2>
+    `);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
