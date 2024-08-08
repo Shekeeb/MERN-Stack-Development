644a87101b5a415a33c1c3cb1fa08389207b6ea1
Shekeeb <shekeebmohammed10@gmailcom>
2024-08-08 19:03:56 +0530

    Promise all, async&await

--- Day25/day25.js
+++ Day25/day25.js
@@ -16,15 +16,15 @@ function getAddress(){
 }
 
 Promise.all([getName(),getAddress()]).then((result)=>{
-    console.log(result)  //It takes 5s to print the result
+    console.log(result)  //It takes 5s to print both the result
 })
 
 
-//Async and Await
-async function user(){                 // it will lead to pending promise, unless using await & async
-    let name=await getName()
-    console.log(name)                  //it takes 3s to print
-    let address=await getAddress()
-    console.log(address)               //it takes another 5s to print after printing name
-}
-user()
+// //Async and Await
+// async function user(){                 // it will lead to pending promise, unless using await & async
+//     let name=await getName()
+//     console.log(name)                  //it takes 3s to print
+//     let address=await getAddress()
+//     console.log(address)               //it takes another 5s to print after printing name
+// }
+// user()
